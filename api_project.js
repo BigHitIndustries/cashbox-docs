efd3f694bc1fdb3f12c4f9979e76c33b90702978
Bradford Knowlton <brad@velpeo.com>
2020-11-21 09:56:21 -0700

    Added static index

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-21T15:37:22.585Z",
+    "time": "2020-11-21T16:56:06.000Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
