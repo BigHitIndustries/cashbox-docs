b77906f04f6cd146e72be94392ec4a06bd15a4b6
Bradford Knowlton <brad@velpeo.com>
2020-11-21 10:30:55 -0500

    version 2.0.1

--- api_project.js
+++ api_project.js
@@ -1,15 +1,15 @@
 define({
+  "name": "Jackpot Documentation",
   "title": "Big Hit Industries Jackpot API Platform",
   "url": "http://127.0.0.1:5000/v2",
-  "name": "jackpot-docs",
-  "version": "1.5.0",
+  "version": "2.0.1",
   "description": "Documentation for Jackpot API platform",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-21T15:15:09.736Z",
+    "time": "2020-11-21T15:30:47.025Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
