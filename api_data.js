be52fef41a2e0e8761ad3770859fff3457a75fdd
Bradford Knowlton <brad@velpeo.com>
2020-11-28 23:26:51 -0700

    useful message

--- api_data.js
+++ api_data.js
@@ -1,138 +1,6 @@
 define({ "api": [
   {
     "type": "get",
-    "url": "/",
-    "title": "Blog Posts",
-    "name": "BlogPosts",
-    "group": "Blog",
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "posts",
-            "description": "<p>all blog posts.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "../cashbox-platform/cashbox/blog.py",
-    "groupTitle": "Blog"
-  },
-  {
-    "type": "get",
-    "url": "/create",
-    "title": "Create New Blog Post",
-    "name": "CreatePost",
-    "group": "Blog",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "Number",
-            "optional": false,
-            "field": "id",
-            "description": "<p>Users unique ID.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "../cashbox-platform/cashbox/blog.py",
-    "groupTitle": "Blog"
-  },
-  {
-    "type": "get",
-    "url": "/:id/delete",
-    "title": "Delete Blog Post",
-    "name": "DeletePost",
-    "group": "Blog",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "Number",
-            "optional": false,
-            "field": "id",
-            "description": "<p>Users unique ID.</p>"
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "firstname",
-            "description": "<p>Firstname of the User.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "lastname",
-            "description": "<p>Lastname of the User.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "../cashbox-platform/cashbox/blog.py",
-    "groupTitle": "Blog"
-  },
-  {
-    "type": "get",
-    "url": "/:id/update",
-    "title": "Update Blog Post",
-    "name": "UpdatePost",
-    "group": "Blog",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "Number",
-            "optional": false,
-            "field": "id",
-            "description": "<p>Users unique ID.</p>"
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "firstname",
-            "description": "<p>Firstname of the User.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "lastname",
-            "description": "<p>Lastname of the User.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "../cashbox-platform/cashbox/blog.py",
-    "groupTitle": "Blog"
-  },
-  {
-    "type": "get",
     "url": "/hello",
     "title": "Hello World",
     "name": "HelloWorld",
@@ -238,6 +106,42 @@ define({ "api": [
   },
   {
     "type": "get",
+    "url": "/login",
+    "title": "Login with user",
+    "name": "LoginUser",
+    "group": "User",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Users unique ID.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Authentication token.</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "../cashbox-platform/cashbox/auth.py",
+    "groupTitle": "User"
+  },
+  {
+    "type": "get",
     "url": "/auth/login",
     "title": "Login with user",
     "name": "LoginUser",
@@ -271,42 +175,6 @@ define({ "api": [
             "optional": false,
             "field": "lastname",
             "description": "<p>Lastname of the User.</p>"
-          }
-        ]
-      }
-    },
-    "version": "0.0.0",
-    "filename": "../cashbox-platform/cashbox/auth.py",
-    "groupTitle": "User"
-  },
-  {
-    "type": "get",
-    "url": "/login",
-    "title": "Login with user",
-    "name": "LoginUser",
-    "group": "User",
-    "parameter": {
-      "fields": {
-        "Parameter": [
-          {
-            "group": "Parameter",
-            "type": "Number",
-            "optional": false,
-            "field": "id",
-            "description": "<p>Users unique ID.</p>"
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "token",
-            "description": "<p>Authentication token.</p>"
           }
         ]
       }
